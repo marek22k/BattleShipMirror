683036e9ba1c77fa1b4e4b455ff080bc12012007
Marek Küthe <m.k@mk16.de>
2024-11-27 13:35:02 +0000

    Add more documentation

--- src/battleship/network/commands/Chat.java
+++ src/battleship/network/commands/Chat.java
@@ -1,12 +1,26 @@
 package battleship.network.commands;
 
+/**
+ * Repräsentiert ein CHAT-Paket
+ */
 public class Chat implements Command {
     private final String message;
 
+    /**
+     * Erstellt aus einem CHAT-Paket in seiner genormten Übertragungsform als String
+     * eine Repräsentation des Paketes.
+     * 
+     * @param command Das CHAT-Paket
+     * @return
+     */
     public static Chat fromString(String command) {
         return new Chat(command.strip());
     }
 
+    /**
+     * Erstellt eine Repräsentation des CHAT-Paketes
+     * @param message Die Nachricht, welche das Paket übermittelt.
+     */
     public Chat(String message) {
         this.message = message;
     }
