959e5eb4c9abae05a6f0ac4b6418b086128479fe
Marek Küthe <m.k@mk16.de>
2024-12-03 15:07:58 +0000

    Add documentation

--- src/battleship/ui/playgroundMatrix/FireListener.java
+++ src/battleship/ui/playgroundMatrix/FireListener.java
@@ -1,5 +1,14 @@
 package battleship.ui.playgroundMatrix;
 
+/**
+ * Listener, welcher aufgerufen wird, wenn auf ein Feld gefeuert wird.
+ */
 public interface FireListener {
+    /**
+     * Methode, welche beim feuern aufgerufen wird
+     *
+     * @param e Feuerevent, welches die Koordinaten des Feldes, auf was gefeuert
+     *          wurde, enthält
+     */
     void fire(FireEvent e);
 }
