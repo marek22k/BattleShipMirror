f2708d49f6b091e9034b75851f7b9dc7735eaf0c
Marek Küthe <m.k@mk16.de>
2024-12-03 12:47:13 +0000

    Add more documentation

--- src/battleship/sound/Sound.java
+++ src/battleship/sound/Sound.java
@@ -5,17 +5,32 @@ import java.util.logging.Logger;
 
 import battleship.Constants;
 
+/**
+ * Globale Klasse, welche für das Laden, Verwalten und Abspielen von Tönen und
+ * ähnlichem zuständig ist.
+ */
 public final class Sound {
+    /*
+     * Die verschiedenen Töne:
+     */
     private static SoundFile hit1;
     private static SoundFile hit2;
     private static SoundFile water;
     private static SoundFile victory;
+
     private static Logger logger;
 
+    /**
+     * Sound kann nicht instanziiert werden
+     */
     private Sound() {
         throw new UnsupportedOperationException("Sound cannot be instantiated");
     }
 
+    /**
+     * Lädt den Sound, indem die Repräsentationen der einzelnen Töne erstellt
+     * werden. In diesem Schritt selbst wird keine Datei geladen.
+     */
     public static void loadSound() {
         logger = Logger.getLogger(Sound.class.getName());
         logger.setLevel(Constants.logLevel);
@@ -32,22 +47,40 @@ public final class Sound {
         }
     }
 
+    /**
+     * Spiele den Ton Hit 1
+     */
     public static void playHit1() {
         play(hit1);
     }
 
+    /**
+     * Spiele den Ton Hit 2
+     */
     public static void playHit2() {
         play(hit2);
     }
 
+    /**
+     * Spiele den Sieges-Ton
+     */
     public static void playVictory() {
         play(victory);
     }
 
+    /**
+     * Spiele des Wasser-Ton
+     */
     public static void playWater() {
         play(water);
     }
 
+    /**
+     * Spiele eine bestimmte Sound-Datei ab. Sollte dies fehlschlagen wird eine
+     * Fehlermeldung geloggt. Es wird kein Fehler geworfen.
+     * 
+     * @param sf Die Sound-Datei, welche abgespielt werden soll.
+     */
     private static void play(SoundFile sf) {
         if (Constants.SOUND) {
             try {
